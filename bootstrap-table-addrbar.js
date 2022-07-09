32723963e098149ef7e38ad8906c7c888f952a42
general <generals.space@gmail.com>
2022-07-09 12:47:53 +0800

    更新: 添加popstate监听, 点击浏览器前进/后退按钮, 可以加载历史数据.

--- bootstrap-table-addrbar.js
+++ bootstrap-table-addrbar.js
@@ -29,6 +29,23 @@
         return null;
     }
 
+    function _buildParam(_bt, option){
+        var _prefix     = option.addrPrefix || '';
+        var _defaults   = _bt.defaults;
+
+        // 优先级排序: 优先从地址栏获取, 没有读取到时使用用户的指定值, 用户未定义时采用默认值
+        var _limit  = _GET(_prefix + 'limit');
+        var _page   = _GET(_prefix + 'page');
+        var _order  = _GET(_prefix + 'order');
+        var _sort   = _GET(_prefix + 'sort');
+        var _search = _GET(_prefix + 'search');
+        option.pageSize     = _limit ? parseInt(_limit) : (option.pageSize || _defaults.pageSize);
+        option.pageNumber   = _page ? parseInt(_page) : (option.pageNumber || _defaults.pageNumber);
+        option.sortOrder    =  _order ? _order : (option.sortOrder || _defaults.sortOrder);
+        option.sortName     = _sort ? _sort : (option.sortName || 'id');
+        option.searchText   =  _search ? _search : (option.searchText || _defaults.searchText);
+    }
+
     /*
     * function: 根据给定参数生成url地址
     * var dic = {name: 'genreal', age: 24}
@@ -67,37 +84,28 @@
     }
 
     /*
-     * 实例化bootstrapTable对象时, 合并用户选项
-     */
+    * 实例化bootstrapTable对象时, 合并用户自定义选项
+    */
     var _bootstrapTable = $.fn.bootstrapTable;
     $.fn.bootstrapTable = function(option){
+        // 这里是为 $().bootstrapTable('refreshOptions') 这种"静态函数"用法准备的.
+        // 只有 option 是 object 的时候, 才算是构造函数.
         if(!(typeof option === 'object')){
             // 直接传入arguments不行, 因为它是一个类数组的对象, 
             // 而bt对参数的处理是面向原生参数列表的.
-            // 目前来看, bt还没有超过2个参数的方法, 暂时先这么用着
+            // 目前来看, bt还没有超过2个参数的方法, 暂时先这么用着.
             return _bootstrapTable.call(this, arguments[0], arguments[1]);
         }
 
         // 拥有addrbar选项并且其值为true的才会继续执行
-        if(!(option.hasOwnProperty('addrbar') && option.addrbar == true))
+        if(!(option.hasOwnProperty('addrbar') && option.addrbar == true)) {
             return _bootstrapTable.call(this, option);
+        }
         // 标志位, 初始加载后关闭
         option._addrbarInit = true;
         var _prefix = option.addrPrefix || '';
-        var _defaults = _bootstrapTable.defaults;
 
-        // 优先级排序: 优先从地址栏获取, 没有读取到时使用用户的指定值, 用户未定义时采用默认值
-        var _limit = _GET(_prefix + 'limit');
-        var _page = _GET(_prefix + 'page');
-        var _order = _GET(_prefix + 'order');
-        var _sort = _GET(_prefix + 'sort');
-        var _search = _GET(_prefix + 'search');
-        option.pageSize = _limit ? parseInt(_limit) : (option.pageSize || _defaults.pageSize);
-        option.pageNumber = _page ? parseInt(_page) : (option.pageNumber || _defaults.pageNumber);
-        option.sortOrder =  _order ? _order : (option.sortOrder || _defaults.sortOrder);
-        option.sortName = _sort ? _sort : (option.sortName || 'id');
-        option.searchText =  _search ? _search : (option.searchText || _defaults.searchText);
-
+        _buildParam(_bootstrapTable, option);
         option._onLoadSuccess = option.onLoadSuccess;
         option.onLoadSuccess = function(data){
             // md, 这里的this是option是什么鬼(好像貌似就是option...ok, 当我没说)
@@ -118,6 +126,17 @@
 
             if(option._onLoadSuccess) option._onLoadSuccess.call(this, data);
         };
+
+        var _window = this;
+        window.addEventListener('popstate', function(){
+            // 这里的 this 其实是全局的 window 对象, 
+            // eventListener 回调函数内部的 this 为触发的元素对象,
+            // 这两个是不同的.
+            // console.log(this);
+            // console.log(_window);
+            _buildParam(_bootstrapTable, option);
+            _bootstrapTable.call(_window, 'refreshOptions', option);
+        });
 
         return _bootstrapTable.call(this, option);
     };
